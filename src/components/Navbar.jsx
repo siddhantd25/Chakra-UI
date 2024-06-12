265523bbf628239cb6e18bb58c0340d0e883fa3d
Sidd <siddhantd37@gmail.com>
2024-06-12 12:40:29 +0530

    Cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { Flex, Heading, Box, Text, Button, Spacer, HStack } from "@chakra-ui/rea
 
 export default function Navbar() {
   return (
-    <Flex as="nav" p="10px" mb="60px" alignItems="center">
+    <Flex as="nav" p="10px" mb="40px" alignItems="center">
       <Heading as="h1" fontSize="1.5em">Sidd Tasks</Heading>
       <Spacer />
 
       <HStack spacing="20px"> 
-        <Box bg="gray.200" p="10px 15px" borderRadius="50%">M</Box>
+        <Box bg="gray.200" p="10px 15px" borderRadius="50%">S</Box>
         <Text>siddhantd37@gmail.com</Text>
         <Button colorScheme="purple">Logout</Button>
       </HStack>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -19,37 +19,35 @@ export default function Dashboard() {
   const tasks = useLoaderData()
 
   return (
-    <SimpleGrid spacing={10} minChildWidth={300}>
-      {tasks && tasks.map(task => (
-        <Card key={task.id} borderTop="8px" borderColor="purple.400" bg="white">
+    <SimpleGrid spacing={10} minChildWidth="300px">
+        {tasks && tasks.map(task => (
+          <Card key={task.id} borderTop="8px" borderColor="purple.400" bg="white">
+            <CardHeader>
+                <Flex gap={5}>
+                  <Box w="50px" h="50px">
+                    <Text>SD</Text>
+                  </Box>
+                  <Box>
+                    <Heading as="h3" size="sm">{task.title}</Heading>
+                    <Text>by {task.author}</Text>
+                  </Box>
+                </Flex>
+            </CardHeader>
 
-          <CardHeader color="gray.700">
-            <Flex gap={5}>
-              <Box w="50px" h="50px">
-                <Text>AV</Text>
-              </Box>
-              <Box>
-                <Heading as="h3" size="sm">{task.title}</Heading>
-                <Text>by {task.author}</Text>
-              </Box>
-            </Flex>
-          </CardHeader>
+            <CardBody color="gray.500">
+                <Text>{task.description}</Text>
+            </CardBody>
 
-          <CardBody color="gray.500">
-            <Text>{task.description}</Text>
-          </CardBody>
+            <Divider borderColor="gray.200" />
 
-          <Divider borderColor="gray.200" />
-
-          <CardFooter>
-            <HStack>
-              <Button variant="ghost" leftIcon={<ViewIcon />}>Watch</Button>
-              <Button variant="ghost" leftIcon={<EditIcon />}>Comment</Button>
-            </HStack>
-          </CardFooter>
-
-        </Card>
-      ))}
+            <CardFooter>
+              <HStack>
+                  <Button variant="ghost" leftIcon={<ViewIcon/>}>Watch</Button>
+                  <Button variant="ghost" leftIcon={<EditIcon/>}>Comment</Button>
+              </HStack>
+            </CardFooter>
+          </Card>
+        ))}
     </SimpleGrid>
   )
 }
